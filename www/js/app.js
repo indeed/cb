8aaaf2c8f7b5a6d21fc6727a42c5d998d5081bec
Shun Yu Rao <shunyu.rao@gmail.com>
2015-09-27 23:08:49 -0400

    Work on menu + android platform

--- www/js/app.js
+++ www/js/app.js
@@ -4,67 +4,72 @@
 // 'cb' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'cb.controllers' is found in controllers.js
-angular.module('cb', ['ionic', 'cb.controllers'])
+var app = angular.module('cb', ['ionic', 'cb.controllers', 'cb.services', 'cb.utils', 'ngTouch', 'ngAnimate'])
 
-  .run(function ($ionicPlatform) {
+app.run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    })
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+          .state('app', {
+              url: '/app',
+              abstract: true,
+              templateUrl: 'views/menu.html',
+              controller: 'menuCtrl'
+          })
+
+          .state('app.calendar', {
+              url: '/calendar',
+              views: {
+                  'menuContent': {
+                      templateUrl: 'views/calendar.html'
+                  }
+              }
+          })
+
+          .state('app.classes', {
+              url: '/classes',
+              views: {
+                  'menuContent': {
+                      templateUrl: 'views/classes.html',
+                  }
+              }
+          })
 
 
+          .state('app.news', {
+              url: '/news',
+              views: {
+                  'menuContent': {
+                      templateUrl: 'views/news.html',
+                      controller: 'newsCtrl'
+                  }
+              }
+          })
+
+         .state('app.cougarvision', {
+             url: '/cougarvision',
+             views: {
+                 'menuContent': {
+                     templateUrl: 'views/cougarvision.html',
+                     controller: 'cvCtrl'
+                 }
+             }
+         });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/news');
     });
-  })
-
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'views/menu.html',
-        controller: 'AppCtrl'
-      })
-
-      .state('app.search', {
-        url: '/search',
-        views: {
-          'menuContent': {
-            templateUrl: 'views/search.html'
-          }
-        }
-      })
-
-      .state('app.news', {
-        url: '/news',
-        views: {
-          'menuContent': {
-            templateUrl: 'views/news.html',
-            controller: 'newsCtrl'
-          }
-        }
-      })
-      
-      
-      
-      
-     .state('app.cougarvision', {
-        url: '/cougarvision',
-        views: {
-          'menuContent': {
-            templateUrl: 'views/cougarvision.html'
-          }
-        }
-      });
-  
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/news');
-  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,97 @@
 var app = angular.module('cb.controllers', []);
 
-app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+// Cougarvision view
+app.controller('cvCtrl', function ($scope, $rootScope, $ionicPopover, $ionicModal) {
 
-})
+    $scope.days = [
+    { name: "Monday" },
+    { name: "Tuesday" },
+    { name: "Wednesday" },
+    { name: "Thursday" },
+    { name: "Friday" },
+    ];
 
-app.controller('newsCtrl', function($scope) {
+    $scope.filterDays = [
+    { name: "All episodes", filter: "", selected: true },
+    { name: "Monday show", filter: "Monday", selected: false },
+    { name: "Tuesday show", filter: "Tuesday", selected: false },
+    { name: "Wednesday show", filter: "Wednesday", selected: false },
+    { name: "Thursday show", filter: "Thursday", selected: false },
+    { name: "Friday show", filter: "Friday", selected: false },
+    ];
 
-})
+    $scope.filter = {
+        selected: $scope.filterDays[0]
+    };
 
-.controller('newsItemCtrl', function($scope, $stateParams) {
+    $scope.episodes = [
+        { day: "Monday", date: "Sep 15", views: 420 },
+        { day: "Tuesday", date: "Sep 16", views: 312 },
+        { day: "Wednesday", date: "Sep 17", views: 629 },
+        { day: "Thursday", date: "Sep 18", views: 711 },
+        { day: "Friday", date: "Sep 19", views: 251 },
+    ];
+
+    $ionicModal.fromTemplateUrl('cvFilterModal.html', function (modal) {
+        $scope.modalCtrl = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up',
+        focusFirstInput: true
+    });
+
+    $scope.filterModal = function () {
+        $scope.modalCtrl.show();
+    };
+
 });
 
+// Cougarvision view modal to filter episodes by day
+app.controller('filterModalCtrl', function ($scope) {
 
+    $scope.hideModal = function (selected) {
+        $scope.modalCtrl.hide();
+    };
+
+    $scope.hideModalAction = function (selected) {
+        $scope.modalCtrl.hide();
+        $scope.filter.selected = selected
+    };
+
+});
+
+app.controller('newsCtrl', function ($scope) {
+
+
+});
+
+app.controller('menuCtrl', function ($scope, $ionicHistory, calendarService) {
+
+    $scope.views = [
+        { name: "News", ref: "news", icon: "ion-android-notifications" },
+        { name: "Classes", ref: "classes", icon: "ion-university" },
+        { name: "CougarVision", ref: "cougarvision", icon: "ion-ios-videocam" },
+        { name: "Calendar", ref: "calendar", icon: "ion-android-calendar" },
+    ];
+
+    // Display current day in cycle as menu header
+    $scope.cycleDay = {};
+
+    calendarService.getDayEvents(moment().add(0, "days")).then(function (response) {
+        var events = calendarService.parseEvents(response);
+        $scope.cycleDay = calendarService.getCycleDay(events);
+    });
+
+    // Highlight current view in menu
+    $scope.isViewSelected = function (name) {
+        if ($ionicHistory.currentStateName() == 'app.' + name) {
+            return true
+        } else {
+            return false
+        }
+    };
+
+});
+
+app.controller('newsItemCtrl', function ($scope, $stateParams) {
+});
